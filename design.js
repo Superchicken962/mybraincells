26363a36fb1e0233b8e7a6e326a49a33998deba9
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2021-03-09 15:40:23 +1030

    Update design.js

--- design.js
+++ design.js
@@ -2,3 +2,8 @@ function lol() {
     document.write("<h1 center;>Trolled</h1><img src=https://mybraincells.cf/trolled.png>");
     document.write("<style>body{text-align:center; font-size:72px;}</style>")
 }
+
+function cow() {
+    document.write("<h1 center;>It is a Cow</h1><img src=https://mybraincells.cf/trolled.png>");
+    document.write("<style>body{text-align:center; font-size:72px;}</style>")
+}
