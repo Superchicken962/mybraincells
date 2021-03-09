75334a19eb97d79dc5377e9d1a884fe24d257358
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2021-03-09 15:33:41 +1030

    Update design.js

--- design.js
+++ design.js
@@ -1,4 +1,4 @@
 function lol() {
-  document.write("<h1 style="text-align: center; font-size: 70px;>Trolled</h1><img src="trolled.png">);
-                 alert("Hi");
+    document.write("<h1 center;>Trolled</h1><img src=trolled.png>");
+    document.write("<style>body{text-align:center; font-size:72px;}</style>")
 }
